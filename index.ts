0c069db12ba10e01b96f57b61b33733d2a352d17
Damien Suhard <ptitdam2001@gmail.com>
2016-12-06 12:44:30 +0100

    fix syntax errors and prepare for a npm package

--- index.ts
+++
@@ -1,8 +0,0 @@
-
-export * from './src/app/geolocation/geolocation.module';
-export * from './src/app/geolocation/geocode-config';
-
-export * from './src/app/geolocation/service/google-geolocation.service';
-export * from './src/app/geolocation/service/locale-geolocation.service';
-
-export * from './src/app/geolocation/component/searchbar/searchbar.component';
